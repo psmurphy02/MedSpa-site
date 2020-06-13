cc2b1786c9b10f376a9cd3a743619fe099730203
Patrick Murphy <psmurphy02@gmail.com>
2020-06-13 07:32:30 -0700

    Modal Working

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*$("#scheduleButton").click(function() {
+    $("#scheduleModal").modal();
+});
+
+$("#loginButton").click(function() {
+    $("#loginModal").modal();
+});*/
+
+$(document).ready(function() {
+    $("#scheduleBtn").click(function() {
+        $("#scheduleModal").modal();
+    });
+});
